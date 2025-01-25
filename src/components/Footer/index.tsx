160d139cea4a7ef34a98a970555c3e01e36d6483
name <you@example.com>
2025-01-25 14:30:30 +0300

    Initial layout settings

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import style from 'style.module.scss';
+
+export default function Footer() {
+    return (
+        <footer className={style.footer}>
+            <div className={style.container}>
+                
+            </div>
+        </footer>
+    );
+}
