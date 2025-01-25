09128b1fc4aaa861d7be87987ca099642f8d2854
name <you@example.com>
2025-01-25 20:30:46 +0300

    Footer and Header styles fix

--- src/components/Subtitle/index.tsx
+++ src/components/Subtitle/index.tsx
@@ -1,4 +1,4 @@
-import './style.module.scss';
+import style from './style.module.scss';
 
 interface SubtitleProps {
     title: string;
@@ -7,9 +7,9 @@ interface SubtitleProps {
 
 export default function Subtitle({ title, subtitle }: SubtitleProps) {
     return (
-        <>
+        <div className={style.subtitle}>
             <h5>{subtitle}</h5>
             <h2>{title}</h2>
-        </>
+        </div>
     );
 }
