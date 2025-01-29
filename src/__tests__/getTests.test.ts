5e97a1c99eea43cdc9aee2a080104b3d1f55d752
name <you@example.com>
2025-01-29 17:27:05 +0300

    feat: Get arts from server tests has been created

---
+++ src/__tests__/getTests.test.ts
@@ -0,0 +1,25 @@
+import { describe, expect, test } from 'vitest';
+import { getRandomArts } from '../utils/getRandomArts';
+import { serachByParams } from '../utils/searchByParams';
+import { getArtById } from '../utils/getArtById';
+
+describe('Get arts test', () => {
+    const limit = 5;
+    const page = 2;
+
+    test('Get random count test', async () => {
+        const arts = await getRandomArts(limit, page);
+        expect(arts.length).toBe(5);
+    });
+
+    test('Get search count test', async () => {
+        const searchedArts = await serachByParams('test', page, limit);
+        expect(searchedArts.length).toBe(5);
+    });
+
+    test('Get by id test', async () => {
+        const id = 123123;
+        const art = await getArtById(id);
+        expect(art.id).toBe(id);
+    });
+});
