b508bd0a837f06dbc5f9207ee40c96e59e243af0
name <you@example.com>
2025-02-07 12:01:39 +0300

    fix: sorting functions fix to compile eslint

--- src/utils/sortByEndDate.ts
+++ src/utils/sortByEndDate.ts
@@ -1,5 +1,7 @@
 import { IArtItem } from '@src/types/IArtItem';
 
 export const sortByEndDate = (arts: IArtItem[]): IArtItem[] => {
-    return arts.toSorted((a: IArtItem, b: IArtItem) => a.date_end - b.date_end);
+    const sortedArts = [...arts];
+    sortedArts.sort((a: IArtItem, b: IArtItem) => a.date_end - b.date_end);
+    return sortedArts;
 };
--- src/utils/sortByStartDates.ts
+++ src/utils/sortByStartDates.ts
@@ -1,7 +1,7 @@
 import { IArtItem } from '@src/types/IArtItem';
 
 export const sortByStartDate = (arts: IArtItem[]): IArtItem[] => {
-    return arts.toSorted(
-        (a: IArtItem, b: IArtItem) => a.date_start - b.date_start,
-    );
+    const sortedArts = [...arts];
+    sortedArts.sort((a: IArtItem, b: IArtItem) => a.date_start - b.date_start);
+    return sortedArts;
 };
