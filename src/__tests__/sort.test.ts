d6af20c2bdc55ef71fb77e10ad1de05d8699fe53
name <you@example.com>
2025-02-08 18:11:19 +0300

    fix: Old tests code fix

--- src/__tests__/sort.test.ts
+++ src/__tests__/sort.test.ts
@@ -1,3 +1,4 @@
+import { getArtsFromQuery } from '@src/__tests__/utils/getArtsFromQuery';
 import { IArtItem } from '@src/types/IArtItem';
 
 import { describe, expect, test } from 'vitest';
@@ -9,9 +10,10 @@ import { sortByStartDate } from '../utils/sortByStartDates';
 
 describe('Sort tests', () => {
     test('Sort by alphabet', async () => {
-        const randomArts = await getRandomArts(5, 12);
+        const randomArts = await getArtsFromQuery(getRandomArts(5, 12));
         const sortedArts = sortByAlphabet(randomArts);
-        const sortArts = randomArts.toSorted((a: IArtItem, b: IArtItem) =>
+        const sortArts = [...randomArts];
+        sortArts.sort((a: IArtItem, b: IArtItem) =>
             a.title.localeCompare(b.title),
         );
 
@@ -19,9 +21,10 @@ describe('Sort tests', () => {
     });
 
     test('Sort by date start', async () => {
-        const randomArts = await getRandomArts(5, 12);
+        const randomArts = await getArtsFromQuery(getRandomArts(5, 12));
         const sortedArts = sortByStartDate(randomArts);
-        const sortArts = randomArts.toSorted(
+        const sortArts = [...randomArts];
+        sortArts.sort(
             (a: IArtItem, b: IArtItem) => a.date_start - b.date_start,
         );
 
@@ -29,11 +32,10 @@ describe('Sort tests', () => {
     });
 
     test('Sort by date end', async () => {
-        const randomArts = await getRandomArts(5, 12);
+        const randomArts = await getArtsFromQuery(getRandomArts(5, 12));
         const sortedArts = sortByEndDate(randomArts);
-        const sortArts = randomArts.toSorted(
-            (a: IArtItem, b: IArtItem) => a.date_end - b.date_end,
-        );
+        const sortArts = [...randomArts];
+        sortArts.sort((a: IArtItem, b: IArtItem) => a.date_end - b.date_end);
 
         expect(sortArts).toEqual(sortedArts);
     });
